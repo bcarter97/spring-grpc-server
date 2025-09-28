6987354ff20a0b285fd860e12b353e3c605cf4c0
Ben Carter <benjohn.carter@gmail.com>
2025-09-28 22:23:49 +0100

    Add logging

--- src/main/java/io/github/bcarter97/spring_grpc_server/MetadataInterceptor.java
+++ src/main/java/io/github/bcarter97/spring_grpc_server/MetadataInterceptor.java
@@ -2,9 +2,14 @@ package io.github.bcarter97.spring_grpc_server;
 
 import io.grpc.*;
 import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcGlobalServerInterceptor
 public class MetadataInterceptor implements ServerInterceptor {
+
+  private static final Logger logger = LoggerFactory.getLogger(MetadataInterceptor.class);
+
   @Override
   public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
       ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
@@ -12,11 +17,7 @@ public class MetadataInterceptor implements ServerInterceptor {
     GrpcMetadata grpcMetadata = GrpcMetadata.fromGrpc(headers);
     Context ctx = MetadataContext.with(grpcMetadata);
 
-    System.out.println(
-        "Inbound method="
-            + call.getMethodDescriptor().getFullMethodName()
-            + " grpcMetadata="
-            + grpcMetadata);
+    logger.debug("Inbound method={} grpcMetadata={}", call.getMethodDescriptor().getFullMethodName(), grpcMetadata);
 
     return Contexts.interceptCall(ctx, call, headers, next);
   }
